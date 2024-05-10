26a897c2e1fc46132380f82e1508bc144b87f72e
hy0523 <1159844201@qq.com>
2024-05-10 02:46:42 -0400

    commit

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 PARTITION=Segmentation
 
-GPU_ID=4,5
+GPU_ID=0,1
 dataset=pascal # pascal coco
-exp_name=split3
+exp_name=split0
 
-arch=BAM_final
+arch=DRCNet
 net=resnet50 # vgg resnet50
 
 exp_dir=exp/${dataset}/${arch}/${exp_name}/${net}
@@ -23,9 +23,3 @@ CUDA_VISIBLE_DEVICES=${GPU_ID} python3 -m torch.distributed.launch --nproc_per_n
         --config=${config} \
         --arch=${arch} \
         2>&1 | tee ${result_dir}/train-$now.log
-
-
-#CUDA_VISIBLE_DEVICES=${GPU_ID} python3 train.py \
-#        --config=${config} \
-#        --arch=${arch} \
-#        2>&1 | tee ${result_dir}/train-$now.log
